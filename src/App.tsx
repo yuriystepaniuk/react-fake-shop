9f3ea4a77ef7ec4cc9d6f8b221f6f509c52f6080
yura <yur.stepanyuk@gmail.com>
2022-10-13 20:33:59 +0300

    add optional props with '?'

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,41 @@
 import React from 'react'
 
+type TitleProps = {
+  title: string
+  text?: string //необовязковий параметр
+}
+
+const Title = (props: TitleProps) => (
+  <h1>
+    {props.title} {props.title}
+  </h1>
+)
+
 const title = <h1>Hello React.js</h1>
+
 
 const List = () => {
   return (
-    <React.Fragment>
+    <>
       <ul>
         <li>list item 1</li>
         <li>list item 2</li>
         <li>list item 3</li>
       </ul>
-        <p>Some text</p>
+      <div>
+        <p>Some text 1</p>
         <p>Sone text 2</p>
-    </React.Fragment>
+      </div>
+    </>
   )
 }
 //React Component
-const Title = () => <h1>Hello React component</h1>
 
 
 function App() {
     return <div className="App">
-      <Title />
+      <Title title ="App.js" />
+      <Title title ="Test.js" />
       <List />
        {10+5}
       </div>
