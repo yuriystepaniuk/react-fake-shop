24f1f04e5eb484da0620c7fb957be3dab48d0e18
yura <yur.stepanyuk@gmail.com>
2022-10-13 20:06:33 +0300

    add list component with return

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 const title = <h1>Hello React.js</h1>
 
-const List = () => (
+const List = () => {
+  return (
   <ul>
     <li>list item 1</li>
     <li>list item 2</li>
     <li>list item 3</li>
-  </ul>
-)
+  </ul>)
+}
 
 //React Component
 const Title = () => <h1>Hello React component</h1>
