e642b0d0d3a8a0fce82661d906250362f03640ee
yura <yur.stepanyuk@gmail.com>
2022-10-13 20:17:21 +0300

    add react.fragment instead <div>

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ const title = <h1>Hello React.js</h1>
 
 const List = () => {
   return (
-  <ul>
-    <li>list item 1</li>
-    <li>list item 2</li>
-    <li>list item 3</li>
-  </ul>)
+    <React.Fragment>
+      <ul>
+        <li>list item 1</li>
+        <li>list item 2</li>
+        <li>list item 3</li>
+      </ul>
+        <p>Some text</p>
+        <p>Sone text 2</p>
+    </React.Fragment>
+  )
 }
-
 //React Component
 const Title = () => <h1>Hello React component</h1>
 
