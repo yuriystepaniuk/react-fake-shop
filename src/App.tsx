3fe4e4b76feaf191a5ac83196b098d4e4b93ed99
yura <yur.stepanyuk@gmail.com>
2022-10-13 19:50:27 +0300

    add list for react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
+const title = <h1>Hello React.js</h1>
+
+const list = (
+  <ul>
+    <li>list item 1</li>
+    <li>list item 2</li>
+    <li>list item 3</li>
+  </ul>
+)
+
+
 function App() {
-    return <div className="App"></div>
+    return <div className="App">
+      {title} {list} {10+5}
+      </div>
 }
 
 export default App
