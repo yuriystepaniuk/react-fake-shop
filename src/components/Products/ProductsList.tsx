fd2b82870ad5a3d2b705c8ea015fe3d7d5b886cd
yura <yur.stepanyuk@gmail.com>
2022-10-19 13:03:51 +0300

    add first item

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -1,5 +1,6 @@
-import { Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import React from 'react'
+import ProductListItem from './ProductListItem'
 
 type Props = {}
 
@@ -16,6 +17,17 @@ const ProductsList = (props: Props) => {
             >
                 ProductLists
             </Typography>
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                spacing={4}
+            >
+                <Grid item>
+                    <ProductListItem />
+                </Grid>
+            </Grid>
         </>
     )
 }
