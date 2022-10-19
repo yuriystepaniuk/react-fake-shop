99b0e402c44aeb688ee62626b598808ea63eb270
yura <yur.stepanyuk@gmail.com>
2022-10-19 14:09:19 +0300

    add css hash class

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,5 +1,6 @@
-import { Button, Card, CardActions, CardContent } from '@mui/material'
-import React from 'react'
+import { Button, Card, CardActions, CardContent } from "@mui/material"
+import React from "react"
+import classes from "./ProductListItem.module.css"
 
 type Props = {
     name: string
@@ -19,8 +20,8 @@ const ProductListItem = (props: Props) => {
                 <div>Capacity: {props.capacity} Gb</div>
                 <div>Price: {props.price} $</div>
             </CardContent>
-            <CardActions>
-                <Button>Add to cart</Button>
+            <CardActions className={`${classes["btn-wrap"]}`}>
+                <Button variant="outlined">Add to cart</Button>
             </CardActions>
         </Card>
     )
