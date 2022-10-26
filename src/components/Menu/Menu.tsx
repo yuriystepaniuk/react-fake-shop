7c7bb43aa7b6fe83829e3eb41dcd626331af1965
yura <yur.stepanyuk@gmail.com>
2022-10-27 01:09:10 +0300

    add Menu component

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Button from "@mui/material/Button"
+
+type Props = {}
+
+const Menu = (props: Props) => {
+    return (
+        <>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Login</Button>
+        </>
+    )
+}
+
+export default Menu
--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -2,10 +2,10 @@ import React from "react"
 import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
-import Button from "@mui/material/Button"
 import IconButton from "@mui/material/IconButton"
 import MenuIcon from "@mui/icons-material/Menu"
 import { Container } from "@mui/material"
+import Menu from "components/Menu/Menu"
 
 type Props = {}
 
@@ -14,25 +14,25 @@ const Header = (props: Props) => {
         <>
             <AppBar position="static">
                 <Container maxWidth="lg">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        Logo
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{ flexGrow: 1 }}
+                        >
+                            Logo
+                        </Typography>
+                        <Menu />
+                    </Toolbar>
                 </Container>
             </AppBar>
         </>
