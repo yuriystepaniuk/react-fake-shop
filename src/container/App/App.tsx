6a93c731e473719a796bf64cd5068160ff762247
yura <yur.stepanyuk@gmail.com>
2022-10-27 00:40:39 +0300

    add structure elements in App

---
+++ src/container/App/App.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import CssBaseline from "@mui/material/CssBaseline"
+import Header from "../Header/Header"
+import Main from "../Main/Main"
+import Footer from "../Footer/Footer"
+
+type Props = {}
+export const App = (props: Props) => {
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
+}
+
+export default App
